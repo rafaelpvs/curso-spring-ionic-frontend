9548d32bff6db7913f9126dda4fb73a71d5f6970
Rafael Pinto <rafael12wq@gmail.com>
2021-02-06 15:17:59 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    backetBaseUrl: "https://curso-spring-ionic-rpvs.s3-sa-east-1.amazonaws.com"
 }
