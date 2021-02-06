81d6538759ee2be233703175ebf64bea6c6c21d4
Rafael Pinto <rafael12wq@gmail.com>
2021-02-06 16:40:38 -0300

    Obtendo os dados do formulario de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @IonicPage()
 
@@ -8,7 +9,10 @@ import { IonicPage, MenuController, NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  }
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
@@ -22,6 +26,7 @@ export class HomePage {
   }
 
   login(){
+    console.log(this.creds)
     this.navCtrl.setRoot('CategoriasPage')
   }
 }
