84c23de3ff934899a15126e753fcd71574b200b0
Rafael Pinto <rafael12wq@gmail.com>
2021-02-07 19:00:08 -0300

    Mostrando dados do cliente para pagina de profile

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     baseUrl: "http://localhost:8080",
-    backetBaseUrl: "https://curso-spring-ionic-rpvs.s3-sa-east-1.amazonaws.com"
+    bucketBaseUrl: "https://curso-spring-ionic-rpvs.s3-sa-east-1.amazonaws.com"
 }
